283d6c25576851065afe133d72f90e1f37892faf
ParsaKermaniPour <parsa.kp.2004@gmail.com>
2025-02-12 22:38:21 -0500

    made everything :)

---
+++ script.js
@@ -0,0 +1,11 @@
+function moveRandomElem(elem) {
+  elem.style.position = 'absolute';
+  elem.style.top = Math.floor(Math.random() * 90 + 5) + '%';
+  elem.style.left = Math.floor(Math.random() * 90 + 5) + '%';
+}
+
+const moveRandom = document.querySelector('#move-random');
+
+moveRandom.addEventListener('mouseenter', (e) => {
+  moveRandomElem(e.target);
+});
